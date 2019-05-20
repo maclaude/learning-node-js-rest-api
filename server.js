36c62e9f672710d26215e8a20e49ce67d794de8e
Marc-Antoine C <claude.marcantoine2@gmail.com>
2019-05-20 17:31:15 +0200

    REST APIs, Clients & CORS errors

--- server.js
+++ server.js
@@ -23,6 +23,17 @@ const app = express();
 // ! This middleware should always be placed first
 app.use(bodyParser.json());
 
+// Setting response CORS headers
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 // Routes
 app.use('/feed', feedRoutes);
 
